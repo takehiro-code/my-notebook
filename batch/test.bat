a1c73cb09d775e89dde2f5f63ea1fa45ebc9d742
Takehiro <narutonaruto717@gmail.com>
2021-01-21 03:24:01 -0800

    variables with batch script

---
+++ batch/test.bat
@@ -0,0 +1,8 @@
+
+@REM variable, it can be either double quate or without it
+set class_cat=ClassB
+set seq_name=BasketballDrive
+set class_id=0
+
+echo Class category: %class_cat%, Sequence name: %seq_name%, class ID selected: %class_id%
+echo %class_cat%_%seq_name%_%class_id%
